38db2b01713e1267545309f025c2b8857d0211af
LukJe80 <ProBikeSerwis@poczta.fm>
2026-01-11 06:51:26 +0100

    Create supabaseClient.js
    Add Supabase client

---
+++ panel/supabaseClient.js
@@ -0,0 +1,10 @@
+<script src="https://unpkg.com/@supabase/supabase-js@2"></script>
+<script>
+  const SUPABASE_URL = "https://mzgvxlltlcrvzmtswzql.supabase.co";
+  const SUPABASE_ANON_KEY = "sb_publishable_u6iZP_f82VIoPc450syj4A_fhWmi9Nr";
+
+  window.supabase = supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_ANON_KEY
+  );
+</script>
