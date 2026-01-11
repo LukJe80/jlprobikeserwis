817d432b7fc362f62b9a6c5be0c2ca682e8eee6d
LukJe80 <ProBikeSerwis@poczta.fm>
2026-01-11 07:04:12 +0100

    Update supabaseClient.js

--- panel/supabaseClient.js
+++ panel/supabaseClient.js
@@ -1,10 +1,9 @@
-<script src="https://unpkg.com/@supabase/supabase-js@2"></script>
-<script>
-  const SUPABASE_URL = "https://mzgvxlltlcrvzmtswzql.supabase.co";
-  const SUPABASE_ANON_KEY = "sb_publishable_u6iZP_f82VIoPc450syj4A_fhWmi9Nr";
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
-  window.supabase = supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_ANON_KEY
-  );
-</script>
+const SUPABASE_URL = "https://mzgvxlltlcrvzmtswzql.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_u6iZP_f82VIoPc450syj4A_fhWmi9Nr";
+
+window.supabase = createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
