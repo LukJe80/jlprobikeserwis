b3df4b6dafa369c095db3d07c50c221bc546c5f3
LukJe80 <ProBikeSerwis@poczta.fm>
2026-01-11 10:41:16 +0100

    Update supabaseClient.js

--- panel/supabaseClient.js
+++ panel/supabaseClient.js
@@ -1,10 +1,8 @@
-import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
-
-const SUPABASE_URL = "https://mzgvxlltlcrvzmtswzql.supabase.co";
+const SUPABASE_URL = "https://mzgvlltlcrvzntswzql.supabase.co";
 const SUPABASE_ANON_KEY =
-  "sb_publishable_u6iZP_f82VIoPc450syj4A_fhWmi9Nr";
+  "sb_publishable_u6iZP_f82VI0pc45esyj4A_fhWmi9Nr";
 
-window.supabase = createClient(
+window.supabase = supabase.createClient(
   SUPABASE_URL,
   SUPABASE_ANON_KEY
 );
