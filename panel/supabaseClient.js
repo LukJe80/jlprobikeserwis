ef4dbb018925c8641bd9b1836bed184b983dc43b
LukJe80 <ProBikeSerwis@poczta.fm>
2026-01-18 05:10:16 +0100

    Delete panel directory

--- panel/supabaseClient.js
+++
@@ -1,7 +0,0 @@
-const SUPABASE_URL = 'https://mzgvxlltlcrvzmtswzql.supabase.co'
-const SUPABASE_ANON_KEY = 'sb_publishable_u6iZP_f82VIoPc450syj4A_fhWmi9Nr'
-
-window.supabaseClient = supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_ANON_KEY
-)
